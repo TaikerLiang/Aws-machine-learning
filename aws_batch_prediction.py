0b0427140be87b66e3ffce93fe907b0738e192da
Taiker Liang <liang0816tw@gmail.com>
2015-08-09 17:52:40 +0800

    finish function of batch_prediction

--- aws_batch_prediction.py
+++ aws_batch_prediction.py
@@ -6,11 +6,14 @@ import ujson
 import requests
 import getopt
 import base64
+import os
+import urlparse
 
-s3 = boto3.resource('s3')
-s3_url = "s3://adlocus-ml/testing-1.csv"
+def check_status_complete(status):
+    return True if status == "COMPLETED" else False
 
-def main():
+def main(model_id, ds_id, out_url):
+
     client = boto3.client('machinelearning')
 
     ml = client.get_ml_model(MLModelId=model_id)
@@ -21,34 +24,44 @@ def main():
     
     bp_id = 'bp-' + base64.b32encode(os.urandom(10))
     
-    print bp_id
-    '''
     response = client.create_batch_prediction(
-        BatchPredictionId='string',
-        BatchPredictionName='string',
-        MLModelId='string',
-        BatchPredictionDataSourceId='string',
-        OutputUri='string'
+        BatchPredictionId=bp_id,
+        BatchPredictionName="Batch Prediction for predicting impression",
+        MLModelId=model_id,
+        BatchPredictionDataSourceId=ds_id,
+        OutputUri=out_url
     )
-    ''' 
-    
+
+    print "batch_prediction completed!"
+
 if __name__ == "__main__":
 
     argv = sys.argv[1:]
     try:
-        opts, args = getopt.getopt(argv,"hm:r:",["help", "model_id=", "record="])
+        opts, args = getopt.getopt(argv,"hm:d:o:",["help", "model_id=", "ds_id=", "output="])
     except getopt.GetoptError:
-        print "please type 'python aws_real_time_prediction.py -h' to get more infomation."
+        print "please type 'python aws_batch_prediction.py -h' to get more infomation."
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print 'Usage: python aws_real_time_prediction.py -m <model_id> -r <record>'
+            print 'Usage: python aws_batch_prediction.py -m <model_id> -d <ds_id> -o <output>'
             sys.exit()
         elif opt in ("-m", "--model_id"):
             model_id = arg
+        elif opt in ("-d", "--ds_id"):
+            ds_id = arg
+        elif opt in ("-o", "--output"):
+            out_url = arg
+    
+    parsed_url = urlparse.urlparse(out_url)
 
-    if model_id == '':
-        print "You miss some parameters, please type python aws_real_time_prediction.py to get more infomation."
+    if parsed_url.scheme != 's3':
+        print "s3_output_url must be an s3:// url"
+        sys.exit(2)
 
-    main(model_id)
+    if model_id == '' or ds_id == '':
+        print "You miss some parameters, please type python aws_batch_prediction.py to get more infomation."
+        sys.exit(2)
+
+    main(model_id, ds_id, out_url)
